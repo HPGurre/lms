0e4579c92c3be188bedb2a5ef5c013e0a34b5164
Hans Peter Jensen <HPGurre@gmail.com>
2013-03-05 15:10:21 +0100

    Added content to src/test/java folder

---
+++ lms/src/test/java/dk/itu/gsd/lms/ATestClass.java
@@ -0,0 +1,14 @@
+package dk.itu.gsd.lms;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ATestClass {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
