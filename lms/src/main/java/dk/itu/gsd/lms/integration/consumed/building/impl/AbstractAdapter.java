76475b06f6cedd740f924d43a2a168749fc01cf8
Hans Peter Jensen <HPGurre@gmail.com>
2013-03-13 20:35:41 +0100

    Added abstract adapter

---
+++ lms/src/main/java/dk/itu/gsd/lms/integration/consumed/building/impl/AbstractAdapter.java
@@ -0,0 +1,27 @@
+package dk.itu.gsd.lms.integration.consumed.building.impl;
+
+import java.net.URI;
+
+import javax.ws.rs.core.UriBuilder;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+
+public abstract class AbstractAdapter {
+	protected static URI getBaseURI() {
+		return UriBuilder.fromUri(
+				"http://gsd.itu.dk/api/user/buildinginfo/6/").build();
+	}
+
+	protected WebResource service;
+	
+	public AbstractAdapter() {
+		ClientConfig config = new DefaultClientConfig();
+		Client client = Client.create(config);
+		service = client.resource(getBaseURI());
+	}
+	
+
+}
