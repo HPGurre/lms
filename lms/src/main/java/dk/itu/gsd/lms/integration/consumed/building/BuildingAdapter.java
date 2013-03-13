3ceb4f9a34a32f3f21551788dc7fe21a6d9bd0d8
Hans Peter Jensen <HPGurre@gmail.com>
2013-03-13 20:03:31 +0100

    Small changes in the integration package.

--- lms/src/main/java/dk/itu/gsd/lms/integration/consumed/building/BuildingAdapter.java
+++ lms/src/main/java/dk/itu/gsd/lms/integration/consumed/building/BuildingAdapter.java
@@ -1,7 +1,10 @@
 package dk.itu.gsd.lms.integration.consumed.building;
 
+import dk.itu.gsd.lms.model.Building;
+
 public interface BuildingAdapter {
 
 	public String getBuildings();
+	public Building getBuildingInfo();
 
 }
--- lms/src/main/java/dk/itu/gsd/lms/integration/consumed/building/impl/BuildingAdapterImpl.java
+++ lms/src/main/java/dk/itu/gsd/lms/integration/consumed/building/impl/BuildingAdapterImpl.java
@@ -1,33 +1,23 @@
 package dk.itu.gsd.lms.integration.consumed.building.impl;
 
-import java.net.URI;
-
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.UriBuilder;
 
 import org.springframework.stereotype.Component;
 
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.api.client.config.ClientConfig;
-import com.sun.jersey.api.client.config.DefaultClientConfig;
-
 import dk.itu.gsd.lms.integration.consumed.building.BuildingAdapter;
+import dk.itu.gsd.lms.model.Building;
 
 @Component("buildingAdapter")
-public class BuildingAdapterImpl implements BuildingAdapter {
-	
+public class BuildingAdapterImpl extends AbstractAdapter implements
+		BuildingAdapter {
 
-	private static URI getBaseURI() {
-		return UriBuilder.fromUri(
-				"http://gsd.itu.dk/api/user/buildinginfo/6/").build();
-	}
 	public String getBuildings() {
-		ClientConfig config = new DefaultClientConfig();
-		Client client = Client.create(config);
-		WebResource service = client.resource(getBaseURI());
+		return service.accept(MediaType.APPLICATION_JSON).get(String.class).toString();
+	}
 
-		 return service.accept(MediaType.APPLICATION_JSON).get(String.class)
-		.toString();
+	@Override
+	public Building getBuildingInfo() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
