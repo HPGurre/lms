9a163d6aa7354d636ac354758d6085201e945e7a
Hans Peter Jensen <HPGurre@gmail.com>
2013-03-18 12:48:46 +0100

    Building, room and floors should not be created as tables in the MySQL(can be found tin ds.xml/datasource.properties) when you start and deploy to server.

--- lms/src/main/java/dk/itu/gsd/lms/model/Floor.java
+++ lms/src/main/java/dk/itu/gsd/lms/model/Floor.java
@@ -2,10 +2,38 @@ package dk.itu.gsd.lms.model;
 
 import java.util.List;
 
-public class Floor {
-	private SecurityMode securityMode;
-	private List<Room> rooms;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+@Entity
+@Table(name = "floor")
+public class Floor extends HibernateModel {
+	@Column(name = "security_mode")
+	@Enumerated(EnumType.STRING)
+	private SecurityMode securityMode;
+	
+	@javax.persistence.ElementCollection
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "foreign_floor_id")
+	private List<Room> rooms;
+	
+	@LazyCollection(LazyCollectionOption.FALSE)
+	@ManyToOne
+	@JoinColumn(name = "foreign_building_id")
+	private Building building;
+	
+	
+	@Column(name = "foreign_floor_id", unique = true, nullable = false)
 	private Long foreignFloorID;
 	
 }
--- lms/src/main/java/dk/itu/gsd/lms/model/Room.java
+++ lms/src/main/java/dk/itu/gsd/lms/model/Room.java
@@ -1,11 +1,34 @@
 package dk.itu.gsd.lms.model;
 
+import java.io.Serializable;
 import java.util.List;
 
-public abstract class Room{
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+@Entity
+@Table(name = "room")
+public abstract class Room extends HibernateModel implements Serializable{
+	@Column(name = "security_mode")
+	@Enumerated(EnumType.STRING)
 	private SecurityMode securityMode;
-	private List<Room> adjecentRooms;
 	
+	//private List<Room> adjecentRooms;
+	
+	@LazyCollection(LazyCollectionOption.FALSE)
+	@ManyToOne
+	@JoinColumn(name = "foreign_floor_id")
+	private Floor floor;
+	
+	@Column(name = "foreign_room_id", unique = true, nullable = false)
 	private Long foreignRoomID;
 
 }
