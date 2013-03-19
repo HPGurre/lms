ac4f7e04cce99263ae380cdf59a81f614180e3ee
Hans Peter Jensen <HPGurre@gmail.com>
2013-03-19 11:48:40 +0100

    Added skeleton test of buildingService.

---
+++ lms/src/test/java/dk/itu/gsd/lms/services/BuildingServiceTest.java
@@ -0,0 +1,22 @@
+package dk.itu.gsd.lms.services;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "/application-context.xml" })
+public class BuildingServiceTest {
+
+	@Autowired
+	private BuildingService buildingService;
+
+	@Test
+	public void test() {
+		assertEquals("Audit user did not match expected.", 1, 1);
+	}
+}
