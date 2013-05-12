a547ad483ac426787f30720455deee412b761e85
Hans Peter Jensen <HPGurre@gmail.com>
2013-05-12 03:46:14 +0200

    Updated rules.xls

--- lms/src/main/java/dk/itu/gsd/lms/model/LightingBlock.java
+++ lms/src/main/java/dk/itu/gsd/lms/model/LightingBlock.java
@@ -22,7 +22,7 @@ public class LightingBlock extends HibernateModel implements Serializable {
 	@LazyCollection(LazyCollectionOption.FALSE)
 	@javax.persistence.ElementCollection
 	@OneToMany(cascade = CascadeType.ALL)
-	@JoinColumn(name = "lightingblock_room_id")
+	@JoinColumn(name = "lightingblock_id")
 	private List<Room> rooms;
 
 	public List<Room> getRooms() {
